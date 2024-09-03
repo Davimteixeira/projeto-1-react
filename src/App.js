221bab5196e9fa4d73690094fcad168dccae2f1f
Davimteixeira <daviteixeira077@gmail.com>
2024-09-03 13:42:36 -0300

    renderizando na tela

--- src/App.js
+++ src/App.js
@@ -4,45 +4,40 @@ import { Component } from 'react';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Davi Magalhães",
-      counter: 0
-    };
-  }
-
-  handlePClick = () => {
-    this.setState({ name: 'Júnior' });
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "O titulo 1",
+        body: "O corpo 1"
+      },
+      {
+        id: 2,
+        title: "O titulo 2",
+        body: "O corpo 2"
+      },
+      {
+        id: 3,
+        title: "O titulo 3",
+        body: "O corpo 3"
+      },
+    ]
   }
 
   render() {
   
-    const {name, counter } = this.state;
+    const { posts } = this.state;
   
   return (
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p onClick={this.handlePClick}>
-               {name} {counter}
-              </p>
-              <a
-                onClick={this.handleAClick}
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Este é o link 
-              </a>
-            </header>
+            {posts.map(post => 
+              (
+                <div key={post.id}>
+                  <h1>{post.title}</h1>
+                  <p>{post.body}</p>
+                </div>
+              )
+            )}
           </div>
         );
       }
